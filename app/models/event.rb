73520efc38c63dc8397314dfac9067063ccf6db5
ddiieeggoo b <zkpgmi@gmail.com>
2023-12-12 17:38:26 +0100

    saving

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,25 @@
 class Event < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
 
   def content
     client = OpenAI::Client.new
     chaptgpt_response = client.chat(parameters: {
                                       model: ENV.fetch["MODEL"],
-                                      messages: [{ role: "user", content: "Je veux une liste de trente cadeaux pour #{liens}, cette personne est de sexe #{genre} , cette personne aime #{interests[0]}, #{interests[1]} et #{interests[2]}, le cadeau sera offert à l’occasion de #{occasion}, mon budget se situe entre #{budget_min} et #{budget_max}. Cette personne est susceptible d'être intéressée par #{self.custom_interest} Je veux que le résultat soit intégré dans un array." }]
+                                      messages: [{ role: "user", content: "Je veux une liste de trente cadeaux pour #{liens}, cette personne est de sexe #{genre} , cette personne aime #{interests[0]}, #{interests[1]} et #{interests[2]}, le cadeau sera offert à l’occasion de #{occasion}, mon budget se situe entre #{budget_min} et #{budget_max}. Cette personne est susceptible d'être intéressée par #{self.custom_interest} Je veux que le résultat soit intégré dans un array et que les éléments soient séparés par des double quotes." }]
+                                    })
+    return chaptgpt_response["choices"][0]["message"]["content"]
+  end
+
+mettre un hidden field tag avec le client de la premiere requete
+mettre un hidden field tag avec le client de la premiere requete
+mettre un hidden field tag avec le client de la premiere requete
+
+  def update_content
+
+    client = OpenAI::Client.new
+    chaptgpt_response = client.chat(parameters: {
+                                      model: ENV.fetch["MODEL"],
+                                      messages: [{ role: "user", content: "A partir de la liste générée, prend en compte ce commentaire: #{form.input.valueofthelistform}" }]
                                     })
     return chaptgpt_response["choices"][0]["message"]["content"]
   end
