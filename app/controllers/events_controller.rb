10a27da7777394074b6581d9bddff96a0912c81d
ddiieeggoo b <zkpgmi@gmail.com>
2023-12-12 13:37:01 +0100

    savechangings

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,12 +9,25 @@ class EventsController < ApplicationController
   # Affichage des critères
   def new
     @occasions = occasions
-    @interestes = interests
+    @interests = interests # peut-être mettre les interests dans un array
+    # c'est ici que j'effectue mon piluleprompt ou alors directement dans le modèle
+
+    # client = OpenAI::Client.new
+    # chaptgpt_response = client.chat(parameters: {
+    # model: ENV["MODEL"],
+    # messages: [{ role: "user", content: "Je veux une liste de trente cadeaux pour #{@list[params:target]}, cette personne est de sexe #{@list[params:sexe]} , cette personne aime #{@interests[0]}, #{@interests[1]} et #{@interests[2]}, le cadeau sera offert à l’occasion de #{@list[params:occasions]}, mon budget se situe entre #{@list[params:budget_min]} et #{@list[params:budget_max]}. Je veux que le résultat soit intégré dans un array."}]
+    # })
+    # @content = chaptgpt_response["choices"][0]["message"]["content"]
+
+    # c'est ici que j'effectue mon custominterestprompt, pseudo code :
+    # creer un mini
+    # formulaire dans la view, récupéerer l'input value et l'intégrer dans le piluleprompt
   end
 
   # Affiche de la list
   def show
-    @list = Event.list.first(5)
+    @list = Event.list.first(5) #ici il faudra agir sur @content
+    # c'est ici que j'effectue mon postprompt, je dois trouver un moyen de poursuivre le premier prompt, ou alors le réitérer et ajouter l'input user au nouveau prompt, déplacer tout cela dans l'edit ou l'update
   end
 
   # Modification de la liste de cadeaux
@@ -45,5 +58,6 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:list, :event_name, :event_date, :event_url, :cagnotte_url)
+    # il faut ajouter des choses dans les strong params, budgetmin, max etc
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :user_id
+
+  def content
+    client = OpenAI::Client.new
+    chaptgpt_response = client.chat(parameters: {
+                                      model: ENV.fetch["MODEL"],
+                                      messages: [{ role: "user", content: "Je veux une liste de trente cadeaux pour #{subject}, cette personne est de sexe #{genre} , cette personne aime #{interests[0]}, #{interests[1]} et #{interests[2]}, le cadeau sera offert à l’occasion de #{occasion}, mon budget se situe entre #{budget_min} et #{budget_max}. Je veux que le résultat soit intégré dans un array." }]
+                                    })
+    return chaptgpt_response["choices"][0]["message"]["content"]
+  end
 end
