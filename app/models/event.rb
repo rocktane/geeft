2e39d88fd9aeffd5e9b40909973fa3b0dbbb8da0
ddiieeggoo b <zkpgmi@gmail.com>
2023-12-13 14:53:20 +0100

    i don't know

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ApplicationRecord
               EVJF EVG Remise\ de\ diplôme Juste\ comme\ ça]
   INTERESTS = %w[Musique Sport Nature Art Voyage Lecture Cuisine Technologie Mode Bien-être Cosmétique Humour Cinéma
           Jardinage Jeux-vidéo Langues\ étrangères Astronomie Bricolage Danse Théatre Spectacle Histoire
-          Psychologie Développement\ personnel Sptiritualité Astrologie]
+          Psychologie Développement\ personnel Sptiritualité Astrologie Finance Design Vins/Spiritueux Automobile]
   LIENS = %w[Parent Petit·e-ami·e Frère Soeur Enfant Collègue Grand-parent Cousin·e Oncle Tante Beau-parent
           Beau-frère Belle-soeur Neveu Nièce Petit-enfant BFF Ami·e Conjoint·e Connaissance Patron·ne
           Parrain Marraine Filleul·e Professeur·e Moi-même]
