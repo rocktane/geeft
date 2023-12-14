529fcd8acf1660fff9e96a5975cf129188c3e648
Yohan Gouiran <y.gouiran@gmail.com>
2023-12-14 16:43:37 +0100

    shuffle ok avec animations

--- app/javascript/controllers/shuffle_controller.js
+++ app/javascript/controllers/shuffle_controller.js
@@ -8,27 +8,22 @@ export default class extends Controller {
   }
 
   update(event) {
-    // console.log(event.target.parentNode);
-    // const array = Array.from(event.target.parentNode.classList[1])
-    // const value = event.target.parentNode.classList[1]
-    // const classe = Array.from(value.classList)
-    // console.log(value.classList, array.class)
-
-    // console.log('first-hidden', this.hiddenTargets[0].parentNode.classList.remove("d-none"));
-    // this.hiddenTargets[0].setAttribute("data","visible")
-    // console.log('target-hidden', this.hiddenTargets[0].attributes[1].value = "visible")
-    event.target.parentNode.classList.remove('add-transition')
     event.target.parentNode.classList.add('remove-transition')
+    event.target.parentNode.classList.remove('add-transition')
 
     setTimeout(() => {
-
-      event.target.parentNode.remove()
-
+      const myDiv = event.target.parentNode
+      const myDivChild = event.target.parentNode.firstChild
+      myDiv.style.opacity = '0';
       let firstOccurence = true;
+
       this.itemTargets.forEach((item) => {
         if (firstOccurence && item.parentNode.classList.contains('d-none')) {
-          item.parentNode.classList.add('add-transition')
-          item.parentNode.classList.remove('d-none')
+          myDivChild.innerHTML = item.innerHTML
+          item.parentNode.remove()
+          myDiv.classList.remove('remove-transition')
+          myDiv.classList.add('add-transition')
+          myDiv.style.opacity = '100';
           firstOccurence = false;
         }
       })
