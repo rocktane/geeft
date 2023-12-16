52d6837b177c70c9a321855f4f2fec8e84e72a8d
Yohan Gouiran <y.gouiran@gmail.com>
2023-12-16 16:43:00 +0100

    commit with body-category

--- app/javascript/controllers/custom_interest_controller.js
+++ app/javascript/controllers/custom_interest_controller.js
@@ -11,6 +11,6 @@ export default class extends Controller {
     event.preventDefault()
 const custom = this.inputInterestTarget.value;
 console.log(this.divInterestTarget.firstElementChild);
-this.divInterestTarget.firstElementChild.insertAdjacentHTML("beforeend", `<div class="form-check"><input class="form-check-input check_boxes optional pill-interest" data-action="click->checkboxes#connect" type="checkbox" checked="checked" value="${custom}" name="event[subject][]" id="event_subject_${custom}"></input><label class="form-check-label collection_check_boxes" for="event_subject_${custom}">${custom}</label>`)
+this.divInterestTarget.lastElementChild.insertAdjacentHTML("beforeend", `<div class="form-check"><input class="form-check-input check_boxes optional pill-interest" data-action="click->checkboxes#connect" type="checkbox" checked="checked" value="${custom}" name="event[subject][]" id="event_subject_${custom}"></input><label class="form-check-label collection_check_boxes" for="event_subject_${custom}">${custom}</label>`)
   }
 }
