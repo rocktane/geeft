72c775dd73b17c5cc156aa75b048ec06e65a74dc
Yohan Gouiran <y.gouiran@gmail.com>
2023-12-19 16:57:02 +0100

    javascript 5 items en cours

--- app/javascript/controllers/shuffle_controller.js
+++ app/javascript/controllers/shuffle_controller.js
@@ -8,38 +8,48 @@ export default class extends Controller {
   }
 
   update(event) {
-    event.target.closest('.gift-item-row').classList.add('remove-transition')
-    event.target.closest('.gift-item-row').classList.remove('add-transition')
-
       const myDiv = event.target.closest('.gift-item-row')
       const myDivChild = event.target.closest('.gift-item-row').firstChild
       const dNone = document.querySelectorAll('.gift-list-row, .d-none').length
       myDiv.style.opacity = '0';
-      let firstOccurence = true;
 
-      this.itemTargets.forEach((item) => {
-        if (firstOccurence && item.parentNode.classList.contains('d-none')) {
-          myDivChild.innerHTML = item.innerHTML
-          item.parentNode.remove()
-          myDiv.classList.remove('remove-transition')
-          myDiv.classList.add('add-transition')
-          if (dNone === 1) {
-            this.disableButton()
-          }
-          myDiv.style.opacity = '100';
-          firstOccurence = false;
+      let firstOccurrence = this.itemTargets.find((item) => {
+        return item.parentNode.classList.contains('d-none');
+      });
+
+      if (firstOccurrence) {
+        myDivChild.innerHTML = firstOccurrence.innerHTML;
+        firstOccurrence.parentNode.remove();
+        myDiv.classList.add("animation");
+
+        if (dNone === 1) {
+          this.disableButton();
         }
-      })
+      }
+
+      this.newList()
+
+      myDiv.style.opacity = '1';
+      setTimeout(() => {
+        myDiv.classList.remove("animation");
+      }, 500);
   }
 
   disableButton() {
     const shuffleIcons = document.querySelectorAll('.shuffle')
 
     shuffleIcons.forEach((icon) => {
-      console.log(icon);
       icon.style.color = "grey";
       icon.style.pointerEvents = 'none';
     });
-    // shuffleIcons.all.element.disabled = true;
   };
+
+  newList() {
+    const cinqPremiersElements = this.itemTargets.slice(0, 5);
+    let new_list = []
+     cinqPremiersElements.forEach((item) => {
+      new_list.push(item.innerText)
+    })
+    console.log(new_list);
+  }
 }
