31e4caf9a71e2f3185ae50dfa116fb0b4e58cfb2
Yohan Gouiran <y.gouiran@gmail.com>
2023-12-21 13:02:10 +0100

    push dylane banzai

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  config.credentials.domain_url = ENV['DOMAIN']
+
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
